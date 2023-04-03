0175bbb300b05f598646965b87e2fda962dd4364
Tadeu <tadeu.fco@gmail.com>
2023-04-03 12:38:38 -0300

    Front funcionando junto com o Back (Validade + Unidade)

---
+++ scripts.js
@@ -0,0 +1,153 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+  let url = 'http://127.0.0.1:5000/produtos';
+  fetch(url, {
+    method: 'get',
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      data.produtos.forEach(item => insertList(item.nome, item.quantidade, item.valor, item.unidade, item.validade))
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Chamada da função para carregamento inicial dos dados
+  --------------------------------------------------------------------------------------
+*/
+getList()
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para colocar um item na lista do servidor via requisição POST
+  --------------------------------------------------------------------------------------
+*/
+const postItem = async (inputProduct, inputQuantity, inputPrice, inputUnit, inputDate) => {
+
+
+  const body = { nome: inputProduct, quantidade: inputQuantity, valor: inputPrice, unidade: inputUnit, validade: inputDate }
+
+  let url = 'http://127.0.0.1:5000/produto';
+  fetch(url, {
+    method: 'post',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body)
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão close para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+  let span = document.createElement("span");
+  let txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  parent.appendChild(span);
+}
+
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão close
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+  let close = document.getElementsByClassName("close");
+  // var table = document.getElementById('myTable');
+  let i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+      if (confirm("Você tem certeza?")) {
+        div.remove()
+        deleteItem(nomeItem)
+        alert("Removido!")
+      }
+    }
+  }
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para deletar um item da lista do servidor via requisição DELETE
+  --------------------------------------------------------------------------------------
+*/
+const deleteItem = (item) => {
+  console.log(item)
+  let url = 'http://127.0.0.1:5000/produto?nome=' + item;
+  fetch(url, {
+    method: 'delete'
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para adicionar um novo item com nome, quantidade e valor 
+  --------------------------------------------------------------------------------------
+*/
+const newItem = () => {
+  let inputProduct = document.getElementById("newInput").value;
+  let inputQuantity = document.getElementById("newQuantity").value;
+  let inputPrice = document.getElementById("newPrice").value;
+  let inputUnit = document.getElementById("newUnit").value;
+  let inputDate = document.getElementById("newDate").value;
+  console.log(inputDate)
+  if (inputProduct === '') {
+    return alert("Escreva o nome de um item!");
+  }
+  if (isNaN(inputQuantity) || isNaN(inputPrice)) {
+    return alert("Quantidade e valor precisam ser números!");
+  }
+
+  insertList(inputProduct, inputQuantity, inputPrice, inputUnit, inputDate)
+  postItem(inputProduct, inputQuantity, inputPrice, inputUnit, inputDate)
+  alert("Item adicionado!")
+
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (nameProduct, quantity, price, unit, date) => {
+  var item = [nameProduct, quantity, price, unit, date]
+  var table = document.getElementById('myTable');
+  var row = table.insertRow();
+
+  for (var i = 0; i < item.length; i++) {
+    var cel = row.insertCell(i);
+    cel.textContent = item[i];
+  }
+  insertButton(row.insertCell(-1))
+  document.getElementById("newInput").value = "";
+  document.getElementById("newQuantity").value = "";
+  document.getElementById("newPrice").value = "";
+  document.getElementById("newUnit").value = "";
+  document.getElementById("newDate").value = "";
+
+  removeElement()
+}
